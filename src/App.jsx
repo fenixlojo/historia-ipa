2d93dfe0cc6a12b9d241a0dcf2ce1ea5e4605787
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2022-04-06 13:28:30 -0300

    copyright

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import Navbar from "./Views/Navbar/Navbar";
 import Main from "./Views/MainMenu/Main";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
+import { GitHub } from "@mui/icons-material";
 
 function App() {
   return (
@@ -10,6 +11,17 @@ function App() {
       <Container>
         <Main />
       </Container>
+      <Box sx={{ color: "gray", position: "fixed", bottom: 0 }}>
+        <center>
+          Copyright Santiago Gonzalez Lojo 2022.{" "}
+          <GitHub
+            sx={{ cursor: "pointer" }}
+            onClick={() =>
+              window.open("https://github.com/fenixlojo", "_blank")
+            }
+          />
+        </center>
+      </Box>
     </div>
   );
 }
