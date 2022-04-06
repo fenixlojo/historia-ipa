8a5e2d6e9dc90d226fd82c79ab1cb4db19361b3b
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2022-04-06 15:58:56 -0300

    change to drive

--- src/documentos.js
+++ src/documentos.js
@@ -6,34 +6,39 @@ const documentos = [
     autor: "Romualdo Ibáñez. Cristian González.",
   },
   {
-    url: "https://cfe.schoology.com/attachment/2292584438/source/96c531361155b726fb4f95408e8ca82f.pdf",
+    url: "https://drive.google.com/file/d/13qHth0E2WViqi7S4ftdOnIOKHRsR9SI8/view?usp=sharing",
     titulo: "De la alfabetización académica a la alfabetización disciplinar",
     autor: "Navarro.",
   },
   {
-    url: "https://hfjeudldkafzfyracfef.supabase.co/storage/v1/object/public/documentos/Alan_Bryan_Poblamiento_originario.pdf",
+    url: "https://drive.google.com/file/d/1lnptQVxvKQ1LjjGpgU9lsIF1orR0MXo3/view?usp=sharing",
     titulo: "Alan Bryan Poblamiento originario",
     autor: "Alan Bryan",
   },
   {
-    url: "https://hfjeudldkafzfyracfef.supabase.co/storage/v1/object/public/documentos/Diversidad+geografica+y+unidad+cultural.pdf",
+    url: "https://drive.google.com/file/d/1JeT_z_4-6gpqjaNPPPzSf0f79PKXh7TY/view?usp=sharing",
     titulo: "Diversidad geografica y unidad cultural",
     autor: "Lorenzo Ochoa , Edith Ortiz Días y Gerardo Gutiérrez",
   },
   {
-    url: "https://hfjeudldkafzfyracfef.supabase.co/storage/v1/object/public/documentos/Mena.+La+humilde+aventura..pdf",
+    url: "https://drive.google.com/file/d/11wurFdfF5vS02KaoRVL_JZHjn2aKdqZS/view?usp=sharing",
     titulo: "La humilde aventura de conocer. El caso del poblamiento americano",
     autor: "Francisco Mena",
   },
   {
-    url: "https://hfjeudldkafzfyracfef.supabase.co/storage/v1/object/public/documentos/Tesis+doctoral+de+Tropea.pdf",
+    url: "https://drive.google.com/file/d/1WCSJp9VZAvIxdC77xW5uRQ-Gwor97QcO/view?usp=sharing",
     titulo: "Poblamiento de América : Nuevas perspectivas para un antiguo debate",
     autor: "Tropea",
   },
   {
-    url: "https://hfjeudldkafzfyracfef.supabase.co/storage/v1/object/public/documentos/memorias_ancestrales_cat.pdf",
+    url: "https://drive.google.com/file/d/1Bm6WBYdUMJG1Fu5sg6Kc71_rPKUs6ggJ/view?usp=sharing",
     titulo: "Memorias ancestrales. Arte y Arqueología en el Uruguay",
     autor: "Ana María Bello y Roberto Bracco",
+  },
+  {
+    url: "https://drive.google.com/file/d/1-R6_g08GrSOOEvMMvSO-aZWbPkZhWpZ5/view?usp=sharing",
+    titulo: "Prehistoria Las primeras etapas de la humanidad",
+    autor: "Sergio Ripoll López",
   },
 ];
 
