43a75ef270f7d186cc29c9781d43771c157ef298
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2022-04-06 13:32:49 -0300

    new book

--- src/documentos.js
+++ src/documentos.js
@@ -5,6 +5,11 @@ const documentos = [
       "Alfabetización Disciplinar en la Formación Inicial Docente. Leer y escribir para aprender.",
     autor: "Romualdo Ibáñez. Cristian González.",
   },
+  {
+    url: "https://cfe.schoology.com/attachment/2292584438/source/96c531361155b726fb4f95408e8ca82f.pdf",
+    titulo: "De la alfabetización académica a la alfabetización disciplinar",
+    autor: "Navarro.",
+  },
 ];
 
 export default documentos;
