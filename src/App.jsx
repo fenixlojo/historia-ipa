2f4c15a2ed51bd1bc33cca3753d8e0a75df62b1e
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2022-04-11 13:12:51 -0300

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./Views/Navbar/Navbar";
 import Main from "./Views/MainMenu/Main";
 import { Container } from "@mui/material";
 import Footer from "./Views/Footer/Footer";
+import ReactGA from "react-ga";
 
 function App() {
+  React.useEffect(() => {
+    const googleAnalytics = async () => {
+      await ReactGA.initialize("G-5NZ5NHSY71");
+      await ReactGA.pageview(window.location.pathname + window.location.search);
+    };
+    googleAnalytics();
+  }, []);
   return (
     <div className="App">
       <Navbar />
