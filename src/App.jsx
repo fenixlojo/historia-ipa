99da689136eaf27452effc2da603d9ab7502f1f0
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2022-04-11 13:23:04 -0300

    update react-ga

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Navbar from "./Views/Navbar/Navbar";
 import Main from "./Views/MainMenu/Main";
 import { Container } from "@mui/material";
 import Footer from "./Views/Footer/Footer";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 function App() {
   React.useEffect(() => {
     const googleAnalytics = async () => {
       await ReactGA.initialize("G-5NZ5NHSY71");
-      await ReactGA.pageview(window.location.pathname + window.location.search);
+      await ReactGA.send(window.location.pathname + window.location.search);
     };
     googleAnalytics();
   }, []);
