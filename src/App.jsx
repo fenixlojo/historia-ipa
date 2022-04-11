c0eb9995ab997d5548955d8c532e02b1ee01814d
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2022-04-11 13:27:21 -0300

    google a in header in public html

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import Navbar from "./Views/Navbar/Navbar";
 import Main from "./Views/MainMenu/Main";
 import { Container } from "@mui/material";
 import Footer from "./Views/Footer/Footer";
-import ReactGA from "react-ga4";
 
 function App() {
-  React.useEffect(() => {
-    const googleAnalytics = async () => {
-      await ReactGA.initialize("G-5NZ5NHSY71");
-      await ReactGA.send(window.location.pathname + window.location.search);
-    };
-    googleAnalytics();
-  }, []);
   return (
     <div className="App">
       <Navbar />
